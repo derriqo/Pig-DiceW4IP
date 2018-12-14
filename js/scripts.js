b27de29cae1a6f1d98886602e0d775c28970de92
derrrick william <williamderrick100@gmail.com>
2018-12-14 17:39:15 +0300

    Modified the index.html,scripts.js files.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function player (one,two) {
+  this.oneName = one;
+  this.twoName = two;
+}
+
+
+$(document).ready(function){
+ $("form#newplayer").submit(function(event)
+ event.preventDefault();
+
+var  inputte
+
+
+}
